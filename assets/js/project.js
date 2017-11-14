f7392cfc01f60ef7a0b0cfe9abc3be2b74890dc1
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-11-14 12:25:34 -0500

    trying to troubleshoot errors

---
+++ assets/js/project.js
@@ -0,0 +1,13 @@
+// Initialize Foundation
+jQuery( document ).ready( function( $ ) {
+	$( document ).foundation();
+});
+
+// Initialize sliders
+jQuery( function( $ ) {
+
+	$( '.js-slider' ).slick({
+		dots: true
+	});
+
+});
